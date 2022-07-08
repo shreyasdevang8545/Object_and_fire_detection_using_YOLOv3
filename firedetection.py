39a386e864ed055cef37c3160b411e29f6b07086
SHREYAS S Y <84521735+shreyasdevang8545@users.noreply.github.com>
2022-07-08 22:08:34 +0530

    Add files via upload

---
+++ firedetection.py
@@ -0,0 +1,24 @@
+import cv2
+import playsound
+
+vid = cv2.VideoCapture(0)
+fire_cascade = cv2.CascadeClassifier('fire_detection_cascade_model.xml')
+
+while True:
+    _, frame = vid.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # To convert frame into gray color
+    fire = fire_cascade.detectMultiScale(frame, 1.2, 5) # to provide frame resolution
+#fire detection program
+    for (x,y,w,h) in fire:
+        cv2.rectangle(frame,(x-20,y-20),(x+w+20,y+h+20),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+        print("Fire detected")
+
+    cv2.imshow("Image", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+        
+vid.release()
+cv2.destroyAllWindows()
